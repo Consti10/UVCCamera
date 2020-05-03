e8bd9100ac65f74f738c28f5ed71399f8c8a5c85
Consti10 <geierconstantinabc@gmail.com>
2020-05-03 23:55:03 +0200

    move all needed stuff into the new application, leave old one as untouched as possible. WORKS !

--- usbcameratestx/src/main/java/consti10/usbcameratestx/MainActivity.java
+++ usbcameratestx/src/main/java/consti10/usbcameratestx/MainActivity.java
@@ -4,16 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.serenegiant.usb.XTest;
 
 public class MainActivity extends AppCompatActivity {
-    //XTest xTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        XTest.nativeHello("BLA");
     }
 }
